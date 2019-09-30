d06ac83a8f8d83eb1a8321e7725449c4453c9a65
Kristoffer Mårtensson <kristoffermartensson94@gmail.com>
2019-09-30 17:50:48 +0200

    Added more tests

--- spec/robotSpec.js
+++ spec/robotSpec.js
@@ -12,24 +12,31 @@ describe('RobotController', function() {
     }));
 
     describe('robot', function() {
-        it('Tests robot initial startposition', function() {
+        it('Test robot initial startposition', function() {
             var $scope = $rootScope.$new()
             var controller = $controller('RobotController', {$scope: $scope});
             expect($scope.x).toEqual(0);
             expect($scope.y).toEqual(0);
             });
-        // it('Test robot new startposition', function(){
-        //     var $scope = $rootScope.$new()
-        //     var controller = $controller('RobotController', {$scope: $scope});
 
-        // });
-        // it('Test robot new startposition', function(){
-        //     var $scope = $rootScope.$new()
-        //     var controller = $controller('RobotController', {$scope: $scope});
-        // });
-        // it('Test robot new startposition', function(){
-        //     var $scope = $rootScope.$new()
-        //     var controller = $controller('RobotController', {$scope: $scope});
-        // });
+        it('Test robot new startposition', function(){
+            var $scope = $rootScope.$new()
+            var controller = $controller('RobotController', {$scope: $scope});
+            $scope.startX = 4;
+            $scope.startY = 3;
+            $scope.handleStartPosition();
+            expect($scope.x).toEqual(4);
+            expect($scope.y).toEqual(3);
+         });
+
+        it('Test robot input', function(){
+            var $scope = $rootScope.$new()
+            var controller = $controller('RobotController', {$scope: $scope});
+            $scope.x = 1;
+            $scope.y = 2;
+            $scope.inputText = "HGHGGHGHG";
+            $scope.handleInput();
+            expect($scope.output).toEqual("1 3 N");
+        });
     });
 });
