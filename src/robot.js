5ccc3c709971a9c92eb0a69772646d6507730b0c
Kristoffer Mårtensson <kristoffermartensson94@gmail.com>
2019-10-01 14:59:50 +0200

    Frontend for roomchoices

--- src/robot.js
+++ src/robot.js
@@ -1,109 +1,129 @@
-angular.module('robotApp', [])
-  .controller('RobotController', function RobotController($scope) {
-    var direction;
+var app = angular.module('robotApp', [])
+app.controller('RobotController', function RobotController($scope) {
+  var direction;
+  $scope.x = 0;
+  $scope.y = 0
+  
+  $scope.handleStartPosition = function() {
+    console.log(typeof($scope.startX));
+    console.log("startx: " + $scope.startX + "starty: " + $scope.startY);
+    if (!isNaN($scope.startX) && !isNaN($scope.startY)){
+    $scope.x = $scope.startX;
+    $scope.y = $scope.startY;
+    } else {
+      $scope.output = "Incorrect startposition"
+    }
+    console.log("x: " + $scope.x + "y: " + $scope.y);
+  }
+
+  $scope.handleReset = function(){
     $scope.x = 0;
-    $scope.y = 0
-    
-    $scope.handleStartPosition = function() {
-      console.log(typeof($scope.startX));
-      console.log("startx: " + $scope.startX + "starty: " + $scope.startY);
-      if (!isNaN($scope.startX) && !isNaN($scope.startY)){
-      $scope.x = $scope.startX;
-      $scope.y = $scope.startY;
+    $scope.y = 0;
+    direction = 0;
+    $scope.output = "";
+    document.getElementById('startX').value = '';
+    document.getElementById('startY').value = '';
+    document.getElementById('input').value = '';
+  }
+
+  $scope.handleInput = function() {
+    direction = 0;
+    var input = $scope.inputText;
+    for (var i= 0; i < input.length; i++){
+      console.log(input.charAt(i));
+      switch(input.charAt(i)){
+        case "G": 
+        case "F":
+          handleForward()
+          break;
+        case "H":
+        case "R": 
+          handleRight()
+          break;
+        case "V":
+        case "L": 
+          handleLeft()
+          break;
+        default: 
+          handleWrongInput()
+          return;
+      }
+    }
+    $scope.output = $scope.x + " " + $scope.y + " " + robotDirection();
+  }
+
+  function handleForward(){
+    switch(direction){
+      case 0:
+        $scope.y--
+        break;
+      case 1:
+        $scope.x++
+        break;
+      case 2:
+        $scope.y++
+        break;
+      case 3:
+        $scope.x--
+        break;
+      default:
+        break;
+      
+    }
+  }
+
+  function handleRight(){
+      if (direction == 3){
+          direction = 0
       } else {
-        $scope.output = "Incorrect startposition"
+          direction++
       }
-      console.log("x: " + $scope.x + "y: " + $scope.y);
-    }
+  }
 
-    $scope.handleReset = function(){
-      $scope.x = 0;
-      $scope.y = 0;
-      direction = 0;
-      $scope.output = "";
-      document.getElementById('startX').value = '';
-      document.getElementById('startY').value = '';
-      document.getElementById('input').value = '';
-    }
-
-    $scope.handleInput = function() {
-      direction = 0;
-      var input = $scope.inputText;
-      for (var i= 0; i < input.length; i++){
-        console.log(input.charAt(i));
-        switch(input.charAt(i)){
-          case "G": 
-          case "F":
-            handleForward()
-            break;
-          case "H":
-          case "R": 
-            handleRight()
-            break;
-          case "V":
-          case "L": 
-            handleLeft()
-            break;
-          default: 
-            handleWrongInput()
-            return;
-        }
+  function handleLeft(){
+      if (direction == 0){
+          direction = 3
+      } else {
+          direction--
       }
-      $scope.output = $scope.x + " " + $scope.y + " " + robotDirection();
-    }
+  }
 
-    function handleForward(){
-      switch(direction){
-        case 0:
-          $scope.y--
-          break;
-        case 1:
-          $scope.x++
-          break;
-        case 2:
-          $scope.y++
-          break;
-        case 3:
-          $scope.x--
-          break;
-        default:
-          break;
-        
-      }
-    }
+  function handleWrongInput(){
+      $scope.output = "Incorrect input";
+  }
 
-    function handleRight(){
-        if (direction == 3){
-            direction = 0
-        } else {
-            direction++
-        }
+  function robotDirection(){
+    switch(direction){
+      case 0: 
+        return "N"
+      case 1:
+        return "E"
+      case 2: 
+        return "S"
+      case 3:
+        return "W"
+      default:
+        return "Direction error";
     }
+  }
+});
+app.controller('RoomController', function RoomController($scope) {
 
-    function handleLeft(){
-        if (direction == 0){
-            direction = 3
-        } else {
-            direction--
-        }
-    }
+  $scope.handleQuadratic = function(){
+    document.getElementById("iframe1").src = 'quadraticform.html';
+  }
 
-    function handleWrongInput(){
-        $scope.output = "Incorrect input";
-    }
+  $scope.handleCircular = function(){
+    document.getElementById("iframe1").src = 'circularform.html';
+    console.log("Hello from handlecircular");
+  }
 
-    function robotDirection(){
-      switch(direction){
-        case 0: 
-          return "N"
-        case 1:
-          return "E"
-        case 2: 
-          return "S"
-        case 3:
-          return "W"
-        default:
-          return "Direction error";
-      }
-    }
-  });
+  $scope.handleHeightWidth = function(){
+    console.log("side:" + $scope.width);
+  }
+
+  $scope.handleRadius = function(){
+    console.log("radius:" + $scope.radius);
+  }
+
+});
